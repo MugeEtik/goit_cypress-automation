66ce93598fbf3344231a4bb7797fdb8ed191ccf4
MugeEtik <mugeetik@gmail.com>
2026-03-31 19:06:02 +0300

    feat(ci): add GitHub Actions pipeline with v4 actions

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: 'shfju6',
 
   e2e: {
 
---
+++ cypress/e2e/Goit-Homework/hw17-performance.cy.js
@@ -0,0 +1,2 @@
+// D:\Projects_and_Git-Repos\GoIT_Homeworks\HW-14-19\Performance_Artillery\hw17_homework-perf-test
+
